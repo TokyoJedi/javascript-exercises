2e7ec08b8de11534cd390d5e8a7728168e354c2d
Eric Rodriguez <5630899+TokyoJedi@users.noreply.github.com>
2024-12-03 14:35:20 -0600

    javascript exercises 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function () {
+  const f = arguments[0];
+  let float = (f - 32) * (5 / 9)
+  return float.toFixed(1) * 1
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function () {
+  const c = arguments[0];
+
+  let float = (c * (9 / 5) + 32)
+  return float.toFixed(1) * 1
 };
 
 // Do not edit below this line
