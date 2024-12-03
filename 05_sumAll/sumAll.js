e892206e32b670b5e443da428311f4c3324807ae
Eric Rodriguez <5630899+TokyoJedi@users.noreply.github.com>
2024-12-03 12:32:47 -0600

    Complete javascript-exercises 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    const min = Math.min(num1, num2);
+    const max = Math.max(num1, num2);
+    let final = 0;
+
+    if ((min < 0 || max < 0) || (!Number.isInteger(num1) 
+        || !Number.isInteger(num2))) {
+        return "ERROR"
+    }
+
+    for (i = min; i <= max; i++) {
+        final += i;
+    }
+
+    return final;
+
+
 
 };
 
