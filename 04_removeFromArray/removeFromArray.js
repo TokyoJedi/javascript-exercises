9cb4be71b48a3f8c915a07aff37946a849fe0153
Eric Rodriguez <5630899+TokyoJedi@users.noreply.github.com>
2024-12-02 23:50:07 -0600

    Complete javascript-exercises 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    let finalArray = new Set(array);
+    let start = 0;
+    let toRemove = args;
+    let toRemoveCount = toRemove.length;
+
+    while (start < toRemoveCount) {
+        finalArray.delete(toRemove[start]);
+        start++;
+    }
+    
+    finalArray = Array.from(finalArray);
+    
+    return finalArray
+
 };
 
 // Do not edit below this line
