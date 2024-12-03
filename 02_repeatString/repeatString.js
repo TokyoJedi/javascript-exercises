361d9d5307691c2747e6f020bdcf9c02dbb0fd87
Eric Rodriguez <5630899+TokyoJedi@users.noreply.github.com>
2024-12-02 20:29:16 -0600

    Complete javascript-exercises 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    const stringToRepeat = string;
+    const timesToRepeat = num;
+    let final = '';
 
+    if (timesToRepeat < 0) {
+        return "ERROR";          
+    }
+    
+    for(let i = 1; i <= num; i++) {
+        final += stringToRepeat;
+    }
+    return final;
 };
 
 // Do not edit below this line
