444a71610860f15b9cd0f3540f2cd34a79a07be2
Eric Rodriguez <5630899+TokyoJedi@users.noreply.github.com>
2024-12-03 21:53:28 -0600

    Complete javascript-exercises 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const sanitizedInput = input.replace(/[^\w]/g, "").toLowerCase();
+    let inputToTest = sanitizedInput;
+    let start = 0;
+    let end = sanitizedInput.length;
+    let final = [];
 
+    while (start < end) {
+        [final[start], final[end]] = [inputToTest[end], inputToTest[start]];
+        start++;
+        end--;
+    }
+
+    final = (Object.values(final) + "").replace(/,/g, "");
+
+    if (final == inputToTest) {
+        return true
+    }
+
+    return false
 };
 
 // Do not edit below this line
