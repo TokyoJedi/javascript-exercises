02698cb8c6eccac31c0d008b123278f3e0388a79
Eric Rodriguez <5630899+TokyoJedi@users.noreply.github.com>
2024-12-03 23:04:25 -0600

    Complete javascript-exercises 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
 const getTheTitles = function() {
+    let books = arguments;
+    books = Array.from(books).flat();
+    let final = [];
 
+    for(record of books) {
+        final.push(record.title);
+    }
+
+    return final;
 };
 
 // Do not edit below this line
