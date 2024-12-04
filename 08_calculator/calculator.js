b290cdbc92333bae425850e4e8e60111c9ac5806
Eric Rodriguez <5630899+TokyoJedi@users.noreply.github.com>
2024-12-03 20:17:45 -0600

    Complete javascript-exercises 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,70 @@
-const add = function() {
-	
+const add = function (...args) {
+  const input = args;
+  let final = 0;
+
+  for (i = 0; i < input.length; i++) {
+    final += input[i];
+  }
+  return final
 };
 
-const subtract = function() {
-	
+const subtract = function (...args) {
+  let array = args;
+  let final;
+
+  array.forEach((v, k) => {
+    if (k === 0) {
+      final = v;
+      return
+    }
+    final -= (v);
+  });
+
+  return final
 };
 
-const sum = function() {
-	
-};
+const sum = function (...args) {
 
-const multiply = function() {
+  return add(...args.flat());
 
 };
 
-const power = function() {
-	
+const multiply = function (...args) {
+  const input = args.flat();
+  let final = 1;
+
+  for (i = 0; i < input.length; i++) {
+    final *= input[i];
+  }
+
+  return final
 };
 
-const factorial = function() {
-	
+const power = function (...args) {
+  const input = args.flat();
+  let final = input[0] ** input[1];
+
+  return final
+};
+
+const factorial = function (arg) {
+  const LOWEST_FACTOR = 1;
+  let end = arg;
+  let final = LOWEST_FACTOR;
+  let factors = [];
+
+  while (LOWEST_FACTOR <= end) {
+    factors.unshift(end);
+    end--;
+  }
+
+
+  for (i = 0; i < factors.length; i++) {
+    final *= factors[i];
+  }
+
+  return final
+
 };
 
 // Do not edit below this line
