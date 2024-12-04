eaf3db4c1fdea90d801a391879ba99ac46ccd237
Eric Rodriguez <5630899+TokyoJedi@users.noreply.github.com>
2024-12-03 22:51:06 -0600

    Complete javascript-exercises 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (numberInSequence) {
+    let sequence = [1, 1];
+    sanitizedNumberInSequence = parseInt(numberInSequence);
 
+    if (!sanitizedNumberInSequence) return 0
+
+    if (sanitizedNumberInSequence < 0) return "OOPS"
+
+    for (i = 1; i < sanitizedNumberInSequence; i++) {
+        sequence.push(sequence[i - 1] + sequence[i]);
+    }
+
+    return sequence[sanitizedNumberInSequence - 1];
 };
 
 // Do not edit below this line
